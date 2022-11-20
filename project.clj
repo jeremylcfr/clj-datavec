1b9bc1d432acc11c227a41799aaf9557ef36abee
unknown <jeremylecorguille@live.fr>
2022-11-20 13:46:20 +0100

    fix project

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject jeremylcfr/clj-datavec "0.1.0-SNAPSHOT"
+(defproject io.github.jeremylcfr/clj-datavec "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"]
+                 [io.github.jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"]
                  [org.deeplearning4j/deeplearning4j-datavec-iterators "1.0.0-M2.1"]] 
   :profiles {:dev {:source-paths ["repl"]}})
