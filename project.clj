9557f4008f857dddd962f8a3da1120462e910903
JeremyLC <jeremylecorguille@live.fr>
2022-11-27 17:03:35 +0100

    add scm

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.github.jeremylcfr/clj-nd4j "0.1.0-SNAPSHOT"]
-                 [org.deeplearning4j/deeplearning4j-datavec-iterators "1.0.0-M2.1"]] 
+                 [org.deeplearning4j/deeplearning4j-datavec-iterators "1.0.0-M2.1"]]
+  :scm {:name "git" :url "https://github.com/jeremylcfr/clj-datavec"}
   :profiles {:dev {:source-paths ["repl"]}}
   :repl-options {:init-ns repl})
